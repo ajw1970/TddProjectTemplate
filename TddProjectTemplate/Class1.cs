a5406790f7b4056d392c50ab00e695e356c5815d
John Welty <john@weltyautomation.com>
2018-09-29 17:53:13 -0500

    First Deployment

--- TddProjectTemplate/Class1.cs
+++ TddProjectTemplate/Class1.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 $if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
 $endif$using System.Text;
+using Xunit;
+using FluentAssertions;
 
 namespace $safeprojectname$
 {
 	public class Class1
 {
+    [Fact]
+    public void Nothing()
+    {
+
+    }
 }
 }
